2ef4576892bec4be64d08ecce09a3e1d4b518062
Haiyi Zhou <joehaiyi@hotmail.com>
2018-10-13 00:42:32 -0400

    Added Dialog for choosing a existing project

--- app/src/main/java/app/haitech/orderly/MainScreen/Act_MainScreen.java
+++ app/src/main/java/app/haitech/orderly/MainScreen/Act_MainScreen.java
@@ -18,23 +18,35 @@ import android.view.MenuItem;
 import android.support.design.widget.NavigationView;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import app.haitech.orderly.R;
 
-public class Act_MainScreen extends AppCompatActivity {
+public class Act_MainScreen extends AppCompatActivity
+{
     String TAG = "Act_MainScreen";
+    //models
     private DrawerLayout mDrawerLayout;
-    private static String projectName;
-    Toolbar toolbar;
+    public static String projectName = "";
     private Context mContext;
+
+    //Views
+    Toolbar toolbar;
+    TextView tv_no_project;
+    TextView tv_create_project;
+    Button btn_create_new_project;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sty_main_screen);
         mContext=this;
+        InitViews();
         // Toolbar
         toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -48,9 +60,11 @@ public class Act_MainScreen extends AppCompatActivity {
         //Navigation Listener
         NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(
-                new NavigationView.OnNavigationItemSelectedListener() {
+                new NavigationView.OnNavigationItemSelectedListener()
+                {
                     @Override
-                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                    public boolean onNavigationItemSelected(MenuItem menuItem)
+                    {
                         // set item as selected to persist highlight
                         menuItem.setChecked(true);
                         // close drawer when item is tapped
@@ -65,37 +79,57 @@ public class Act_MainScreen extends AppCompatActivity {
 
         //Drawer Listener
         mDrawerLayout.addDrawerListener(
-                new DrawerLayout.DrawerListener() {
+                new DrawerLayout.DrawerListener()
+                {
                     @Override
-                    public void onDrawerSlide(View drawerView, float slideOffset) {
+                    public void onDrawerSlide(View drawerView, float slideOffset)
+                    {
                         // Respond when the drawer's position changes
                     }
 
                     @Override
-                    public void onDrawerOpened(View drawerView) {
+                    public void onDrawerOpened(View drawerView)
+                    {
                         // Respond when the drawer is opened
                     }
 
                     @Override
-                    public void onDrawerClosed(View drawerView) {
+                    public void onDrawerClosed(View drawerView)
+                    {
                         // Respond when the drawer is closed
                     }
 
                     @Override
-                    public void onDrawerStateChanged(int newState) {
+                    public void onDrawerStateChanged(int newState)
+                    {
                         // Respond when the drawer motion state changes
                     }
                 }
         );
+        boolean hasExistingProject = CheckIfHasProject();
+        hasExistingProject=true;
+        if(hasExistingProject)
+        {
+            CreateChooseProjectDialog();
+        }else
+        {
+            tv_no_project.setVisibility(View.VISIBLE);
+            tv_create_project.setVisibility(View.VISIBLE);
+            btn_create_new_project.setVisibility(View.VISIBLE);
+        }
     }
+    //---------------------------------------------------------------------------
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.mn_action_bar, menu);
         return true;
     }
+    //---------------------------------------------------------------------------
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
         switch (item.getItemId()) {
             case android.R.id.home:
                 mDrawerLayout.openDrawer(GravityCompat.START);
@@ -103,56 +137,116 @@ public class Act_MainScreen extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
-
-    // New Project Button Clicked
-    public void OnClickNewProject(View v) {
-        CreateDialog();
+    //--------------------------------------------------------------------------
+    private void InitViews()
+    {
+        tv_create_project = (TextView) findViewById(R.id.tv_createProject);
+        tv_create_project.setVisibility(View.INVISIBLE);
+        tv_no_project = (TextView) findViewById(R.id.tv_noProject);
+        tv_no_project.setVisibility(View.INVISIBLE);
+        btn_create_new_project = (Button) findViewById(R.id.btn_newProject);
+        btn_create_new_project.setVisibility(View.INVISIBLE);
     }
-
+    //---------------------------------------------------------------------------
+    public boolean CheckIfHasProject()
+    {
+        // check in DB if project exist
+        //TODO
+        return false;
+    }
+    //---------------------------------------------------------------------------
+    // New Project Button Clicked
+    public void OnClickNewProject(View v)
+    {
+        CreateNewProjectDialog();
+    }
+    //---------------------------------------------------------------------------
     /**
      * Create the dialog where the user can
      * enter a name for the new project
      */
-    private void CreateDialog()
+    private void CreateNewProjectDialog()
     {
         AlertDialog.Builder builder = new AlertDialog.Builder(mContext);
         LayoutInflater inflater = LayoutInflater.from(mContext);
         View v = inflater.inflate(R.layout.sty_dialog_new_project, null);
-        Button btn_ok = (Button)v.findViewById(R.id.btn_ok);
-        Button btn_cancel = (Button)v.findViewById(R.id.btn_cancel);
-        final EditText et_proj_name = (EditText) v.findViewById(R.id.et_projectName);//enter proj name
-        final AlertDialog dialog = builder.create();
-        dialog.show();
-        dialog.getWindow().setContentView(v);
+        if(v!=null) {
+            Button btn_ok = (Button) v.findViewById(R.id.btn_ok);
+            Button btn_cancel = (Button) v.findViewById(R.id.btn_cancel);
+            final EditText et_proj_name = (EditText) v.findViewById(R.id.et_projectName);//enter proj name
+            final AlertDialog dialog = builder.create();
+            dialog.show();
+            dialog.getWindow().setContentView(v);
 
-        // OK button callback
-        btn_ok.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String name = et_proj_name.getText().toString();
-                if(!name.isEmpty())
-                    toolbar.setTitle(name);
-                dialog.dismiss();
-            }
-        });
+            // OK button callback
+            btn_ok.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    projectName = et_proj_name.getText().toString();
+                    if (!projectName.isEmpty()) {
+                        dialog.dismiss();
+                    }else{
+                        et_proj_name.setHint("Name cannot be empty!");
+                    }
+                }
+            });
 
-        // Cancel button callback
-        btn_cancel.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                dialog.dismiss();
-            }
-        });
+            // Cancel button callback
+            btn_cancel.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    dialog.dismiss();
+                }
+            });
 
-        // On focus change show keyboard
-        // fix the issue where keyboard wont pop up
-        et_proj_name.setOnFocusChangeListener(new View.OnFocusChangeListener() {
-            @Override
-            public void onFocusChange(View view, boolean b) {
-                if(b)
-                    dialog.getWindow()
-                            .clearFlags(WindowManager.LayoutParams.FLAG_ALT_FOCUSABLE_IM);
-            }
-        });
+            // On focus change show keyboard
+            // fix the issue where keyboard wont pop up
+            et_proj_name.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+                @Override
+                public void onFocusChange(View view, boolean b) {
+                    if (b)
+                        dialog.getWindow()
+                                .clearFlags(WindowManager.LayoutParams.FLAG_ALT_FOCUSABLE_IM);
+                }
+            });
+        }
+    }
+    //----------------------------------------------------------------------------------------
+    private void CreateChooseProjectDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(mContext);
+        LayoutInflater inflater = LayoutInflater.from(mContext);
+        View v = inflater.inflate(R.layout.sty_dialog_choose_project, null);
+        if (v != null) {
+            final AlertDialog dialog = builder.create();
+            dialog.show();
+            dialog.getWindow().setContentView(v);
+
+            Button btn_open = (Button) v.findViewById(R.id.btn_open);
+            Button btn_new = (Button) v.findViewById(R.id.btn_new);
+            Spinner spinner = (Spinner) v.findViewById(R.id.spinner);
+            // ini spinner ----
+            String[] mItems = {"Select project...","Project A", "Project B", "Project C","Project C","Project C","Project C","Project C","Project C"};
+            ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, mItems);
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            spinner.setAdapter(adapter);
+            // ----------------
+
+
+            // Open button callback
+            btn_open.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    dialog.dismiss();
+                }
+            });
+
+            // New button callback
+            btn_new.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    dialog.dismiss();
+                }
+            });
+        }
     }
 }
