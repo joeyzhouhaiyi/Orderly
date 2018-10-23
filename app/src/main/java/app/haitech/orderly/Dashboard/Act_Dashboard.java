e844649cd44f3c9be6f5312f250877c86abe417f
joeyzhouhaiyi <joehaiyi@hotmail.com>
2018-10-22 23:55:50 -0400

    Tags Dialog

--- app/src/main/java/app/haitech/orderly/Dashboard/Act_Dashboard.java
+++ app/src/main/java/app/haitech/orderly/Dashboard/Act_Dashboard.java
@@ -6,14 +6,23 @@ import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import org.apmem.tools.layouts.FlowLayout;
 
 import app.haitech.orderly.Inventory.Act_Inventory;
 import app.haitech.orderly.R;
@@ -111,7 +120,7 @@ public class Act_Dashboard extends AppCompatActivity {
                                 break;
 
                             case R.id.nav_tag:
-                                //Open tags management
+                                createTagsManagementDialog();
                                 break;
 
                         }
@@ -145,4 +154,74 @@ public class Act_Dashboard extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
     //--------------------------------------------------------------------------
+    protected void createTagsManagementDialog()
+    {
+        AlertDialog.Builder builder = new AlertDialog.Builder(mContext);
+        LayoutInflater inflater = LayoutInflater.from(mContext);
+        View v = inflater.inflate(R.layout.sty_dialog_tag_management, null);
+        if (v != null) {
+            final AlertDialog dialog = builder.create();
+            dialog.show();
+            dialog.getWindow().setContentView(v);
+            RenderTagViews(v);
+            Button btn_close = (Button) v.findViewById(R.id.btn_close);
+            Button btn_edit = (Button) v.findViewById(R.id.btn_edit);
+            btn_close.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    dialog.dismiss();
+                }
+            });
+            btn_edit.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    //TODO
+                }
+            });
+        }
+
+    }
+    //--------------------------------------------------------------------------
+    protected void RenderTagViews(View v)
+    {
+        final FlowLayout flowLayout = v.findViewById(R.id.flowlayout_tags);
+        LayoutInflater inflater = (LayoutInflater) this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        if(inflater!=null) {
+            //TODO: Replace whole thing by tags
+            for (int i = 0; i < 30; i++) {
+                View childView = inflater.inflate(R.layout.sty_btn_tag, null);
+
+                final LinearLayout bTag = (LinearLayout) childView.findViewById(R.id.btn_tag_style);
+                bTag.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                       //TODO Add on click behaviour, enable Edit button
+                        // bTag.setBackgroundResource(R.drawable.btn_square_regular_pressed);
+                    }
+                });
+                TextView num = (TextView) bTag.findViewById(R.id.tv_tag_item_number);
+                TextView name = (TextView) bTag.findViewById(R.id.tv_tag_item_name);
+                num.setText("" + i);
+                if(i%3== 0)
+                    name.setText("Tag " + i*i);
+                else
+                    name.setText("Tag Name ++" + i * i *15);
+                flowLayout.addView(bTag,i);
+            }
+            //--TD--
+            final ImageButton btn = new ImageButton(flowLayout.getContext());
+            btn.setImageResource(R.drawable.ic_add_circle);
+            btn.setMaxWidth(30);
+            btn.setMaxHeight(30);
+            btn.setBackgroundResource(R.drawable.states_bg_tagnum_round);
+            btn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    //ADD TAG
+                }
+            });
+            flowLayout.addView(btn);
+        }
+
+    }
 }
