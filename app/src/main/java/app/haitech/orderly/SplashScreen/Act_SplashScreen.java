0f058a39e038cf0607224274c2f689b33dab6de6
joey <joehaiyi@hotmail.com>
2018-12-27 08:56:18 +0800

    added progress bar

--- app/src/main/java/app/haitech/orderly/SplashScreen/Act_SplashScreen.java
+++ app/src/main/java/app/haitech/orderly/SplashScreen/Act_SplashScreen.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ProgressBar;
 
 import java.util.ArrayList;
 
